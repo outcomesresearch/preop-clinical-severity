f879e9335a3a7541f407e8575b1317782c99b8e4
jeffrey <piccirilloj1@gmail.com>
2022-12-30 14:53:13 -0600

    Add manual preview / embedded meta tags for Preop Clinical Severity applet

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const webpackParams = {
+  title: 'Clinical Severity Staging System Calculator',
+  description:
+    'Calculate the Clinical Severity Staging System category (alpha, beta, gamma, or delta) from Functional Severity, Comorbidity, Preop weight loss, and TNM Stage values.',
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/preop-clinical-severity/',
+}
+
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/preop-clinical-severity/" : "/",
-};
+    process.env.NODE_ENV === 'production' ? '/preop-clinical-severity/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value
+      })
+      return args
+    })
+  },
+}
